79240b29cb7de23934ddd03a8ad2755e3ae6a5fd
Agustina Echegaray <echegarayagu@gmail.com>
2024-10-23 20:18:15 -0600

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+const navElement = document.querySelector('#theNav');
+const btnElement = document.querySelector('#theButton');
+
+btnElement.addEventListener('click', () => {
+    btnElement.classList.toggle('open');
+    navElement.classList.toggle('open');
+});
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    effect: 'slide',
+    speed: 2000,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    // start autoplay
+    autoplay: {
+        delay: 5000,
+    },
+
+  });
+
+// Current year in footer
+const rightNow = new Date();
+document.querySelector('#year').textContent = rightNow.getFullYear();
